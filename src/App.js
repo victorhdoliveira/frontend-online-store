926dfbb09d1df254f8019da358248da25c0677fa
Nathalia <nathalia.cantarelli@edu.pucrs..br>
2022-10-07 19:58:17 -0300

    Requisito 3

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import './App.css';
 import HomePage from './Pages/HomePage';
+import ShoppingCard from './Pages/ShoppingCard';
 
 function App() {
   return (
     <BrowserRouter>
       <Switch>
         <Route exact path="/" component={ HomePage } />
+        <Route path="/shopping-card" component={ ShoppingCard } />
       </Switch>
     </BrowserRouter>
   );
