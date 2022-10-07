a71c7f64b3796c2b35595edc565fb1bfb3e5c729
othiagomanhaes <othiagomanhaes@gmail.com>
2022-10-07 17:16:30 -0300

    Requisito 1.

--- src/App.js
+++ src/App.js
@@ -1,16 +1,9 @@
 import React, { Component } from 'react';
 import './App.css';
-import { getCategories } from './services/api';
 
 class App extends Component {
-  componentDidMount() {
-    getCategories();
-  }
-
   render() {
-    return (
-      <h1>Trybe</h1>
-    );
+    return <div> </div>;
   }
 }
 
--- src/services/api.js
+++ src/services/api.js
@@ -1,17 +1,18 @@
 export async function getCategories() {
-  const request = await fetch('https://api.mercadolibre.com/sites/MLB/categories#json');
+  const request = await fetch('https://api.mercadolibre.com/sites/MLB/categories');
   const json = await request.json();
-  return console.log(json);
+  return json;
 }
 
-export async function getProductsFromCategoryAndQuery(/* categoryId, query */) {
-  // Implemente aqui! Quando o fizer, descomente os parâmetros que essa função recebe
+export async function getProductsFromCategoryAndQuery(categoryId, query) {
+  if (categoryId && query) {
+    const request = await fetch(` https://api.mercadolibre.com/sites/MLB/search?category=${categoryId}_ID&q=${query}`);
+    const json = await request.json();
+    return json;
+  }
 }
 
 export async function getProductById() {
   // Esta implementação específica não é avaliada, mas pode ajudar você 🙂
   // Atenção: essa função não deverá ser chamada na tela do carrinho de compras.
 }
-
-
-// alterando para fazer commit
