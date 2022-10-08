6ee2a6899dfe897b925f7832183323ed2e8875a3
Nathalia <nathalia.cantarelli@edu.pucrs..br>
2022-10-08 02:25:35 -0300

    Requisito 6

--- src/services/api.js
+++ src/services/api.js
@@ -12,7 +12,8 @@ export async function getProductsFromCategoryAndQuery(categoryId, query) {
   }
 }
 
-export async function getProductById() {
-  // Esta implementação específica não é avaliada, mas pode ajudar você 🙂
-  // Atenção: essa função não deverá ser chamada na tela do carrinho de compras.
+export async function getProductById(categoryId) {
+  const request = await fetch(`https://api.mercadolibre.com/sites/MLB/search?category=${categoryId}`);
+  const json = await request.json();
+  return json;
 }
