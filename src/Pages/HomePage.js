40d29fc786ffb13fc5de748dd11d66819185fdcf
Victor Hugo de Oliveira <47421429+victorhdoliveira@users.noreply.github.com>
2022-10-11 19:34:53 -0300

    Grupo 24 - Requisito 10 (lint 1)

--- src/Pages/HomePage.js
+++ src/Pages/HomePage.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import {
-  getCategories, getProductById, getProductsFromCategoryAndQuery
+  getCategories, getProductById, getProductsFromCategoryAndQuery,
 } from '../services/api';
 
 class HomePage extends React.Component {
