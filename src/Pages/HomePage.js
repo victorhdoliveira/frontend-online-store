9198d3b600832deb6d7b9e600f441b4c367bc53f
Bete.mrt <bete.mrt@gmail.com>
2022-12-31 22:09:27 -0300

    Home Page e Shopping Card ok

--- src/Pages/HomePage.js
+++ src/Pages/HomePage.js
@@ -3,6 +3,7 @@ import { Link } from 'react-router-dom';
 import {
   getCategories, getListItem, getProductById, getProductsFromCategoryAndQuery,
 } from '../services/api';
+import searchIcon from '../imgs/search.png';
 
 class HomePage extends React.Component {
   constructor() {
@@ -32,9 +33,12 @@ class HomePage extends React.Component {
     const { valueInput } = this.state;
     const researchReturn = await getProductsFromCategoryAndQuery(valueInput, valueInput);
     if ((researchReturn === undefined) || (researchReturn.results.length === 0)) {
-      this.setState({ stateResearch: true });
+      this.setState({
+        stateCategory: '',
+        stateResearch: true });
     } else {
       this.setState({
+        stateCategory: '',
         list: researchReturn.results,
         stateResearch: false,
       });
@@ -45,6 +49,8 @@ class HomePage extends React.Component {
     const { stateCategory } = this.state;
     const productsFromCategory = await getProductById(id);
     this.setState({
+      stateResearch: '',
+      valueInput: '',
       productsFromCategory: productsFromCategory.results,
       stateCategory: !stateCategory,
     });
@@ -67,93 +73,127 @@ class HomePage extends React.Component {
     const { list, categories, valueInput, stateResearch,
       productsFromCategory, stateCategory, stateCart } = this.state;
     return (
-      <>
-        <div>
-          { stateCart && <p data-testid="shopping-cart-size">{ getListItem() }</p>}
+      <div className="container_main">
+        <div className="box_header">
+          <div className="header_title">
+            <h1>FRONT-END</h1>
+            <h3>Online Store</h3>
+          </div>
           { list && (
-            <p
-              data-testid="home-initial-message"
+            <p data-testid="home-initial-message">.</p>) }
+          <div className="header_search">
+            <input
+              type="text"
+              data-testid="query-input"
+              value={ valueInput }
+              onChange={ this.addValueInput }
+              placeholder="Digite sua pesquisa"
+            />
+            <button
+              type="button"
+              data-testid="query-button"
+              onClick={ this.getProductsFromCategoryAndQueryAPI }
             >
-              Digite algum termo de pesquisa ou escolha uma categoria.
-            </p>) }
-          <Link to="/shopping-card" data-testid="shopping-cart-button">
-            Carrinho de compras
-          </Link>
-          <ul>
-            { categories.map((categorie) => (
-              <li key={ categorie.id }>
-                <button
-                  type="button"
-                  data-testid="category"
-                  onClick={ () => this.getProductsFromCategoryAP(categorie.id) }
-                >
-                  { categorie.name }
-                </button>
-                <br />
-              </li>
-            ))}
-          </ul>
-          <ul>
-            {stateCategory && productsFromCategory
-              .map((product) => (
-                <li key={ product.id } data-testid="product">
-                  <Link to={ `/product/${product.id}` } data-testid="product-detail-link">
-                    {product.title}
-                    {product.price}
-                  </Link>
-                  <img src={ product.thumbnail } alt={ product.title } />
-                  {product.shipping.free_shipping && <img
-                    data-testid="free-shipping"
-                    src="https://img.irroba.com.br/fluzaoco/catalog/frete1.png"
-                    alt="imagem frete gratis"
-                  />}
-                  <button
-                    id={ product.id }
-                    type="button"
-                    data-testid="product-add-to-cart"
-                    onClick={ () => this.addProductShoppingCard(product) }
-                  >
-                    Adicionar no carrinho
-                  </button>
-                </li>))}
-          </ul>
+              <img src={ searchIcon } alt="pesquisa" />
+            </button>
+          </div>
+          <div className="header_cart">
+            <Link to="/shopping-card" data-testid="shopping-cart-button">
+              { stateCart
+            && (
+              <p
+                data-testid="shopping-cart-size"
+                id="qtdItems"
+              >
+                { getListItem() }
+              </p>)}
+            </Link>
+          </div>
         </div>
-        <div>
-          <input
-            type="text"
-            data-testid="query-input"
-            value={ valueInput }
-            onChange={ this.addValueInput }
-          />
-          <button
-            type="button"
-            data-testid="query-button"
-            onClick={ this.getProductsFromCategoryAndQueryAPI }
-          >
-            Buscar
-          </button>
-          { stateResearch ? <p data-testid="product">Nenhum produto foi encontrado</p> : (
+        <div className="box_content">
+          <div className="categories">
+            <h3>Categorias</h3>
             <ul>
-              { list.map((item) => (
-                <li key={ item.id } data-testid="product">
-                  <Link to={ `/product/${item.id}` } data-testid="product-detail-link">
-                    { item.title }
-                    <br />
-                    { item.price }
-                    <br />
-                    <img src={ item.thumbnail } alt={ item.title } />
-                    <br />
-                    {item.shipping.free_shipping && <img
-                      data-testid="free-shipping"
-                      src="https://img.irroba.com.br/fluzaoco/catalog/frete1.png"
-                      alt="imagem frete gratis"
-                    />}
-                  </Link>
+              { categories.map((categorie) => (
+                <li key={ categorie.id }>
+                  <button
+                    type="button"
+                    data-testid="category"
+                    onClick={ () => this.getProductsFromCategoryAP(categorie.id) }
+                  >
+                    { categorie.name }
+                  </button>
                 </li>
               ))}
-            </ul>)}
+            </ul>
+          </div>
+          <div className="content">
+            { stateResearch
+              ? (<h3 data-testid="product">Nenhum produto foi encontrado</h3>) : (
+                !stateCategory && !stateResearch && (
+                  <div className="items">
+                    {/* <ul> */}
+                    { list.map((item) => (
+                      <section key={ item.id } data-testid="product" className="item">
+                        <Link
+                          to={ `/product/${item.id}` }
+                          data-testid="product-detail-link"
+                        >
+                          { item.title }
+                          { item.price }
+                        </Link>
+                        <img src={ item.thumbnail } alt={ item.title } />
+                        {item.shipping.free_shipping && <img
+                          data-testid="free-shipping"
+                          src="https://img.irroba.com.br/fluzaoco/catalog/frete1.png"
+                          alt="imagem frete gratis"
+                        />}
+                        <button
+                          id={ item.id }
+                          type="button"
+                          data-testid="product-add-to-cart"
+                          onClick={ () => this.addProductShoppingCard(item) }
+                        >
+                          Adicionar no carrinho
+                        </button>
+                      </section>))}
+                    {/* </ul> */}
+                  </div>
+                ))}
+            { !stateResearch && (
+              <div className="items">
+                {/* <ul> */}
+                {stateCategory && productsFromCategory
+                  .map((product) => (
+                    <section key={ product.id } data-testid="product" className="item">
+                      <Link
+                        to={ `/product/${product.id}` }
+                        data-testid="product-detail-link"
+                      >
+                        {product.title}
+                        {product.price}
+                      </Link>
+                      <img src={ product.thumbnail } alt={ product.title } />
+                      {product.shipping.free_shipping && <img
+                        data-testid="free-shipping"
+                        src="https://img.irroba.com.br/fluzaoco/catalog/frete1.png"
+                        alt="imagem frete gratis"
+                      />}
+                      <button
+                        id={ product.id }
+                        type="button"
+                        data-testid="product-add-to-cart"
+                        onClick={ () => this.addProductShoppingCard(product) }
+                      >
+                        Adicionar no carrinho
+                      </button>
+                    </section>))}
+                {/* </ul> */}
+              </div>
+            )}
+          </div>
         </div>
-      </>
+      </div>
     );
   }
 }
--- src/Pages/ShoppingCard.js
+++ src/Pages/ShoppingCard.js
@@ -1,5 +1,9 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import '../styles/shopping_card.css';
+import plus from '../imgs/plus-cart.png';
+import minus from '../imgs/minus-cart.png';
+import del from '../imgs/del-cart.png';
 
 class ShoppingCard extends React.Component {
   constructor() {
@@ -41,15 +45,18 @@ class ShoppingCard extends React.Component {
     });
   };
 
-  mudaQuantItem = ({ target: { name, value } }) => {
+  mudaQuantItem = ({ target: { name, className } }) => {
+    console.log(className);
     const { listShoppingCard } = this.state;
+    console.log(listShoppingCard);
     const id = name;
+    console.log(id);
     listShoppingCard.forEach((arrItem, ind, arr) => {
       if (arrItem[0].id === id) {
-        if (value === 'sum' && arrItem[0].available_quantity > arrItem.length) {
+        if (className === 'sum' && arrItem[0].available_quantity > arrItem.length) {
           arrItem.push(arrItem[0]);
           this.setState({ listShoppingCard: arr });
-        } else if (value === 'sub' && arrItem.length > 1) {
+        } else if (className === 'sub' && arrItem.length > 1) {
           arrItem.shift(arrItem[0]);
           this.setState({ listShoppingCard: arr });
         }
@@ -60,60 +67,96 @@ class ShoppingCard extends React.Component {
   render() {
     const { listShoppingCard } = this.state;
     return (
-      <div>
-        { listShoppingCard.length > 0 && (
-          <Link to="/checkout">
-            <button
-              type="button"
-              data-testid="checkout-products"
-            >
-              Finalizar compra
-            </button>
-          </Link>
-        )}
-        { listShoppingCard.length > 0 ? listShoppingCard.map((item, ind) => (
-          <section
-            key={ ind }
-          >
-            <p data-testid="shopping-cart-product-name">{item[0].title}</p>
-            <img src={ item[0].thumbnail } alt={ item[0].title } />
-            <p>{item[0].price}</p>
-            <p data-testid="shopping-cart-product-quantity">
-              { `Quantidade ${item.length}`}
-            </p>
-            <button
-              type="button"
-              data-testid="remove-product"
-              onClick={ this.removeItem }
-              name={ item[0].id }
-            >
-              X
-            </button>
-            <button
-              name={ item[0].id }
-              type="button"
-              value="sum"
-              onClick={ this.mudaQuantItem }
-              data-testid="product-increase-quantity"
-            >
-              +
-            </button>
-            <button
-              type="button"
-              name={ item[0].id }
-              value="sub"
-              onClick={ this.mudaQuantItem }
-              data-testid="product-decrease-quantity"
-            >
-              -
-            </button>
-          </section>
-        )) : (
-          <p
-            data-testid="shopping-cart-empty-message"
-          >
-            Seu carrinho está vazio
-          </p>)}
+      <div className="container_main_cart">
+        <div className="box_header_cart">
+          <div className="header_title_cart">
+            <h1>FRONT-END</h1>
+            <h3>Online Store</h3>
+          </div>
+        </div>
+        <div className="box_cart">
+          <div className="box_content_cart">
+            { listShoppingCard.length > 0 ? listShoppingCard.map((item, ind) => (
+              <section
+                className="item_cart"
+                key={ ind }
+              >
+                <h3 data-testid="shopping-cart-product-name">{item[0].title}</h3>
+                <img src={ item[0].thumbnail } alt={ item[0].title } />
+                <h3>
+                  R$
+                  {item[0].price}
+                </h3>
+                <h3 data-testid="shopping-cart-product-quantity">
+                  { `Quantidade ${item.length}`}
+                </h3>
+                <div className="buttons_cart">
+                  <button
+                    name={ item[0].id }
+                    type="button"
+                    id="addButton"
+                    value="sum"
+                    onClick={ this.mudaQuantItem }
+                    data-testid="product-increase-quantity"
+                  >
+                    <img
+                      src={ plus }
+                      name={ item[0].id }
+                      alt="increase item"
+                      className="sum"
+                    />
+                  </button>
+                  <button
+                    type="button"
+                    name={ item[0].id }
+                    id="decButton"
+                    value="sub"
+                    onClick={ this.mudaQuantItem }
+                    data-testid="product-decrease-quantity"
+                  >
+                    <img
+                      src={ minus }
+                      name={ item[0].id }
+                      alt="decrease item"
+                      className="sub"
+                    />
+                  </button>
+                  <button
+                    type="button"
+                    data-testid="remove-product"
+                    id="removeButton"
+                    onClick={ this.removeItem }
+                    name={ item[0].id }
+                  >
+                    <img
+                      src={ del }
+                      alt="remove item"
+                      name={ item[0].id }
+                    />
+                  </button>
+                </div>
+              </section>
+            )) : (
+              <h3
+                className="message_cart"
+                data-testid="shopping-cart-empty-message"
+              >
+                Seu carrinho está vazio
+              </h3>)}
+          </div>
+          <div className="box_checkout_cart">
+            { listShoppingCard.length > 0 && (
+              <Link to="/checkout">
+                <button
+                  type="button"
+                  data-testid="checkout-products"
+                >
+                  Finalizar compra
+                </button>
+              </Link>
+            )}
+          </div>
+        </div>
       </div>
     );
   }
